6095f1b415988e7c84f9df2f49bc656e4ab18d93
Lil-Tanatat <tas.tanatat@gmail.com>
2025-09-11 19:11:32 +0700

    first commit

---
+++ src/app/about-us/page.js
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar";
+import AboutUsSection from "../../components/AboutUsSection";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <Navbar />
+      <AboutUsSection />
+    </div>
+  );
+}
